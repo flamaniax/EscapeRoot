0c77408a21a8c25cef6caad6a6edd546f9e2a790
flamaniax <47926821+flamaniax@users.noreply.github.com>
2023-02-04 15:29:59 -0500

    Added animations, and the sprite flips based on direction.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,6 +7,8 @@ public class Movement : MonoBehaviour
     public Rigidbody2D rb; //this is a rigidbody, which we will use to move the character around, and use for our physics system
     public float gravity,jumpHeight,Speed;
 
+    public SpriteRenderer PlayerSprite;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +27,15 @@ public class Movement : MonoBehaviour
         Debug.Log(Input.GetAxis("Horizontal"));
 
         rb.velocity = new Vector2(Input.GetAxis("Horizontal") * Speed,rb.velocity.y);
+
+        if(rb.velocity.x < 0)
+        {
+            PlayerSprite.flipX = true;
+        }
+        else
+        {
+            PlayerSprite.flipX = false;
+        }
 
         rb.velocity += new Vector2(0,-gravity) * Time.deltaTime;
     }
