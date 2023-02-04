da37bef06207f133746aaf71a6744cbd60056a98
flamaniax <47926821+flamaniax@users.noreply.github.com>
2023-02-04 16:53:31 -0500

    Updated the Chessboard PatternBuilder to allow for changing the scale of the tiles

--- Assets/Scripts/ChessPatternSpawner.cs
+++ Assets/Scripts/ChessPatternSpawner.cs
@@ -6,20 +6,30 @@ public class ChessPatternSpawner : MonoBehaviour
 {
     public GameObject WhiteTile,BlackTile;
 
+    public float Width,Height,XOffset,YOffset;
+
+    public Vector2 UScale;
+
     // Start is called before the first frame update
     void Start()
     {
-        for(int x = 0; x<=21; x++)
+        WhiteTile.transform.localScale = UScale;
+        BlackTile.transform.localScale = UScale;
+
+        float ScaleX = WhiteTile.transform.localScale.x;
+        float ScaleY = WhiteTile.transform.localScale.y;
+
+        for(int x = 0; x<=1/ScaleX*Width; x++)
         {
-            for(int y = 0; y<=9; y++)
+            for(int y = 0; y<=1/ScaleY*Height; y++)
             {
                 if(y%2 == x%2)
                 {
-                    Instantiate(WhiteTile,new Vector3((x-10.5f),(y-4.5f),0),Quaternion.identity);
+                    Instantiate(WhiteTile,new Vector3((x-(XOffset*1/ScaleX)-0.5f),(y-(YOffset*1/ScaleY)-0.5f),0)*ScaleX,Quaternion.identity);
                 }
                 else
                 {
-                    Instantiate(BlackTile,new Vector3((x-10.5f),(y-4.5f),0),Quaternion.identity);
+                    Instantiate(BlackTile,new Vector3((x-(XOffset*1/ScaleX)-0.5f),(y-(YOffset*1/ScaleY)-0.5f),0)*ScaleY,Quaternion.identity);
                 }
                 
             }
